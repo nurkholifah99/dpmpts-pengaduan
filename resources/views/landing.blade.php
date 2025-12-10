8f5d2d4d08d54313659739700fc2b143fdcc5d6c
muhammadfaiz19 <mfaiz2727@gmail.com>
2025-12-11 00:19:44 +0700

    chore: remove unnecessary file

--- resources/views/landing.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Selamat Datang di DPMPTSP Cirebon</h1>
-<a href="{{ route('pengaduan.index') }}">Daftar Pengaduan</a>
-<a href="{{ route('tracking.index') }}">Cek Status</a>
-<a href="{{ route('dashboard') }}">Dashboard Petugas</a>
